f263c0efa427451d9e056414c1eeda3deb6510cb
Chris George <74033835+pricelessaditi@users.noreply.github.com>
2023-11-27 10:56:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // script.js
 
+let cart = []; // Array to store added books
+
 function addToCart(title, price) {
-    // You can implement cart functionality here
-    alert(`Added ${title} to the cart. Total Price: $${price}`);
+    cart.push({ title, price });
+    alert(`Added ${title} to the cart. Total Price: $${calculateTotal()}`);
+}
+
+function calculateTotal() {
+    return cart.reduce((total, book) => total + book.price, 0).toFixed(2);
+}
+
+function viewCart() {
+    if (cart.length === 0) {
+        alert("Your cart is empty.");
+    } else {
+        let cartDetails = "Cart Details:\n";
+        cart.forEach((book) => {
+            cartDetails += `${book.title} - $${book.price}\n`;
+        });
+        cartDetails += `Total: $${calculateTotal()}`;
+        alert(cartDetails);
+    }
 }
