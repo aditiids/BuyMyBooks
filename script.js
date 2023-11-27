f03adb465ee261d055deb3d7113e691d442d06b7
Chris George <74033835+pricelessaditi@users.noreply.github.com>
2023-11-27 15:41:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,13 @@ let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
 // Function to add a book to the cart
 function addToCart(title, price, bookId) {
-    cart.push({ title, price });
+    cart.push({ title, price, bookId });
 
     // Save the updated cart to local storage
     localStorage.setItem('cart', JSON.stringify(cart));
 
-    // Add bookId to the cart item for identification
-    cart[cart.length - 1].bookId = bookId;
-
-    alert(`Added ${title} to the cart. Total Price: $${calculateTotal()}`);
+    // Display updated cart
+    viewCart();
 }
 
 // Function to calculate the total price of items in the cart
@@ -48,14 +46,16 @@ function removeFromCart(bookId) {
 function checkout() {
     alert("Checkout functionality goes here!");
     // Add your checkout logic here (e.g., sending the order to a server, processing payment, etc.)
+
+    // After checkout, clear the cart
+    cart = [];
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+    // Display updated cart
+    viewCart();
 }
 
 // Initialize the cart when the page loads
 window.onload = function () {
     viewCart();
-};
-
-// Initialize the cart when the page loads
-window.onload = function () {
-    initCart();
 };
