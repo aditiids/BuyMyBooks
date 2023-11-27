7ba14eb2b81629200958a60305cc693badd4cdf6
Chris George <74033835+pricelessaditi@users.noreply.github.com>
2023-11-27 15:47:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ function addToCart(title, price, bookId) {
     // Save the updated cart to local storage
     localStorage.setItem('cart', JSON.stringify(cart));
 
-    // Display updated cart
-    viewCart();
+    alert(`Added ${title} to the cart. Total Price: ₹${calculateTotal()}`);
 }
 
 // Function to calculate the total price of items in the cart
@@ -26,10 +25,10 @@ function viewCart() {
     } else {
         let cartDetails = "Cart Details:\n";
         cart.forEach((book) => {
-            cartDetails += `${book.title} - $${book.price} 
+            cartDetails += `${book.title} - ₹${book.price} 
                 <button onclick="removeFromCart('${book.bookId}')">Remove</button>\n`;
         });
-        cartDetails += `Total: $${calculateTotal()}`;
+        cartDetails += `Total: ₹${calculateTotal()}`;
         cartDetails += `\n<button onclick="checkout()">Checkout</button>`;
         alert(cartDetails);
     }
@@ -44,18 +43,43 @@ function removeFromCart(bookId) {
 
 // Function to handle the checkout process
 function checkout() {
-    alert("Checkout functionality goes here!");
-    // Add your checkout logic here (e.g., sending the order to a server, processing payment, etc.)
-
-    // After checkout, clear the cart
-    cart = [];
-    localStorage.setItem('cart', JSON.stringify(cart));
-
-    // Display updated cart
-    viewCart();
+    // Redirect to the checkout page
+    window.location.href = 'checkout.html';
 }
 
-// Initialize the cart when the page loads
-window.onload = function () {
-    viewCart();
-};
+// Function to display order summary on the checkout page
+function displayOrderSummary() {
+    const orderSummaryElement = document.getElementById('orderSummary');
+    if (cart.length === 0) {
+        orderSummaryElement.innerHTML = '<p>Your cart is empty.</p>';
+    } else {
+        let orderDetails = '<p>Order Summary:</p>';
+        cart.forEach((book) => {
+            orderDetails += `<p>${book.title} - ₹${book.price}</p>`;
+        });
+        orderDetails += `<p>Total: ₹${calculateTotal()}</p>`;
+        orderSummaryElement.innerHTML = orderDetails;
+    }
+}
+
+// Function to submit the order
+function submitOrder() {
+    if (cart.length === 0) {
+        alert("Your cart is empty. Add items to proceed to checkout.");
+    } else {
+        // Add your logic for submitting the order (e.g., sending data to a server)
+        alert("Order submitted successfully!");
+        
+        // Clear the cart after submitting the order
+        cart = [];
+        localStorage.setItem('cart', JSON.stringify(cart));
+
+        // Redirect to a thank you page or homepage
+        window.location.href = 'thankyou.html';
+    }
+}
+
+// Initialize the checkout page when it loads
+if (window.location.pathname.includes('checkout.html')) {
+    displayOrderSummary();
+}
